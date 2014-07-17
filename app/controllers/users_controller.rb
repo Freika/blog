b5e838819970362dc32dbf907a4dfcee8dee81db
Freika <iamfrey@gmail.com>
2014-07-17 23:04:16 +0700

    Added: no new users anymore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,23 +7,23 @@ class UsersController < ApplicationController
   end
 
   # GET /users/new
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
+  # # POST /users
+  # def create
+  #   @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to root_path, notice: 'User was successfully created.' }
+  #     else
+  #       format.html { render :new }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   def update
