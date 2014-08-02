7088764400482a94672f34882c7dd61de9ef1a92
Freika <iamfrey@gmail.com>
2014-08-03 00:05:47 +0700

    Added: kramdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def full_title(page_title)
-     base_title = "Фрей написал."
-      if page_title.empty?
-       base_title
-      else
-        "#{page_title}, #{base_title}"
-      end
-  end 
+    base_title = 'Фрей написал.'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title}, #{base_title}"
+    end
+  end
+
+  def markdown(source)
+    Kramdown::Document.new(source).to_html.html_safe
+  end
 end
