b130eea0ca9e8ff31cac15da0ee065b0132bb113
Freika <iamfrey@gmail.com>
2014-10-28 22:04:28 +0600

    Registration forbidden

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable
-        # :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable
+        # :recoverable, :rememberable, :registerable, :trackable, :validatable
 end
