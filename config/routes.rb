66cb6837ea15b99ec8e5cafad196b52b237aef4d
Freika <iamfrey@gmail.com>
2014-12-10 06:48:14 +0600

    Removed: robots

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   get 'bonus/digitalocean' => redirect('https://www.digitalocean.com/?refcode=5dcbfa133a56')
   get 'feeds/rss', controller: 'feeds', action: 'rss', format: 'rss', as: :feed
   get 'feeds/sitemap.xml' => 'feeds#sitemap'
-  get 'feeds/robots.txt' => 'feeds#robots', format: 'text'
 end
